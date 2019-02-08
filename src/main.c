f360e3961c3ac6ef1982d04a856bd6e4484c5e28
nidzik <nidzik@student.42.fr>
2019-02-08 22:35:42 +0100

    modify entry point

--- src/main.c
+++ src/main.c
@@ -1,29 +1,40 @@
 #include "woody.h"
 
-// long getImageBase(int fd)
-// {
-// 	Elf64_Ehdr ehdr;
-// 	Elf64_Phdr phdr;
+void find_sect(void *elf, const char * sect)
+{
+	void *elfsave; 
+	Elf64_Shdr *shdr;
+	Elf64_Shdr stable;
+	char *sname;
+	int shnum;
 
-// 	lseek(fd, 0, SEEK_SET);
 
-// 	read(fd, &ehdr, sizeof(ehdr)); // read Elf Header
-// 	int numheaders = ehdr.e_phnum; // how many program headers are there?
-// 	printf("e_phunum = %d (number of headers)\n", numheaders);
-// 	lseek(fd, ehdr.e_phoff, SEEK_SET); // set file offset to first program header as provided by elf.e_phoff
-
-// 	for (int i = 0; i < numheaders; i++) // loop through all program headers
-// 	{
-// 		read(fd, &phdr, sizeof(phdr));
-// 		if (phdr.p_type == 1) // check the p_type for 1 which is PT_LOAD
-// 		{
-// 			printf("ImageBase: %x\n", phdr.p_vaddr); // first header that is a PT_LOAD type contains image base
-// 			break;
-// 		}
-// 	}
-
-// 	return phdr.p_vaddr;
-// }
+//	printf("Program entry: %lx\nPhdr entry : %ld\nShdr entry : %ld \nSize of ELF header : %d \n", elf->e_entry, elf->e_phoff, elf->e_shoff, elf->e_ehsize);
+	elfsave = elf; 
+	shdr = (Elf64_Shdr *)(elf + ((Elf64_Ehdr *)elf)->e_shoff);
+	stable = shdr[((Elf64_Ehdr *)elf)->e_shstrndx];
+	sname = (char *)(elf + stable.sh_offset);
+	
+	for (shnum = 0 ; shnum < ((Elf64_Ehdr *)elf)->e_shnum ; shnum++)
+	{
+		stable = shdr[shnum];
+//		printf ("name sect : %s \t\t|offset : %d \n", sname + stable.sh_name, stable.sh_offset);
+		if (!strcmp(sname + stable.sh_name, sect))
+		{
+			printf ("\nname sect : %s \t| offset : %d \n", sname + stable.sh_name, stable.sh_offset);
+			for (int j = 0; j < stable.sh_size ; j+=4)
+			{
+				printf(" %02x%02x%02x%02x ", *((unsigned char*)elfsave + stable.sh_offset + j),\
+					   *((unsigned char*)elfsave + stable.sh_offset + j+1),\
+					   *((unsigned char*)elfsave + stable.sh_offset + j+2),\
+					   *((unsigned char*)elfsave + stable.sh_offset + j+3));
+				if (!(j % 16) && j != 0)
+					printf("\n");
+				fflush (stdout);
+			}
+		}
+	}	
+}
 
 char *get_file(char *name, off_t *file_size)
 {
@@ -42,7 +53,7 @@ char *get_file(char *name, off_t *file_size)
 		return (NULL);
 	}
 	*file_size = metadata.st_size;
-	if ((file = mmap(0, (size_t)*file_size, PROT_READ,
+	if ((file = mmap(0, (size_t)*file_size, PROT_READ | PROT_WRITE,
 					 MAP_PRIVATE, fd, 0)) == MAP_FAILED)
 	{
 		dprintf(2, "mmap failed");
@@ -59,7 +70,7 @@ int main(int ac, char **av)
 	void *ptr = NULL;
 	Elf64_Ehdr *elf;
 	Elf32_Shdr *shdr;
-
+	
 	if (ac < 2)
 	{
 		dprintf(2, "Usage: %s [FILENAME]\n", av[0]);
@@ -70,7 +81,7 @@ int main(int ac, char **av)
 	if (!file)
 		return (1);
 	printf("Sucess retreiving %s, file size: %zu bytes.\n", av[1], file_size);
-	// read(fd, &elf, sizeof(elf));
+
 	elf = (void *)file;
 
 	// Step 1 : generate our code with the good address
@@ -81,22 +92,29 @@ int main(int ac, char **av)
 	off_t cave_size;
 	off_t entry;
 	entry = find_cave(file, file_size, 0, &cave_size);
+
 	printf("bigest cave entry: %jd, cave size: %jd\n", entry, cave_size);
+	
 	// Step 3 : copy our code (if we found a place, else we )
 	// memcpy(bin + entry, our_code, code_length)
 
-	// getImageBase(fd);
+	/* replace Program entry  (+1 to 'align the shit')*/
+/*
+	((Elf64_Ehdr *)elf)->e_entry = entry + 1;
 
-	/* replace Program entry */
-	// elf->e_entry = 0x41414141;
-
-	// printf("Program entry: %lx\nPhdr entry : %ld\nShdr entry : %ld \nSize of ELF header : %d \n", elf->e_entry, elf->e_phoff, elf->e_shoff, elf->e_ehsize);
+	int fdd = open(av[1], O_RDWR );
+	lseek(fdd,0, SEEK_SET);
+	write(fdd, elf, sizeof(Elf64_Ehdr));
+	lseek(fdd, entry+1, SEEK_SET);
+	char *strr = "\xcc\x80";
+	printf("strr == %s ", strr);
+	write(fdd,strr,3); 
+	close(fdd);
+*/		
+	find_sect(elf,".text");
+	
 	inject_code(file, file_size, entry, cave_size);
-	// lseek(fd, 6654, SEEK_SET);
-	// write(fd, &shdr, sizeof(shdr));
-	// printf("section name : %s \n", shdr.sh_name);
-	//	printf();
-
+	 
 	printf("\nclosing fd, exiting...\n");
 	return 0;
 }
